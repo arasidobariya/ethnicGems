73f93ccebe3f5b56df33ba92734e6790f5afed99
Arasi Dobariya <arasidobariya7@gmail.com>
2023-10-12 16:14:06 -0400

    added drawer in header for responsive layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,57 +1,39 @@
 import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Grid from "@mui/material/Grid";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
+import {
+  AppBar,
+  Grid,
+  Toolbar,
+  Typography,
+  Drawer,
+  ListItemText,
+  List,
+  ListItem,
+  IconButton,
+  Divider,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  ThemeProvider,
+  createTheme,
+  styled,
+  useTheme,
+} from "@mui/material/styles";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import SearchIcon from "@mui/icons-material/Search";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { Badge, IconButton, autocompleteClasses } from "@mui/material";
+import { useState } from "react";
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
 
 function Header() {
-  const Search = styled("div")(({ theme }) => ({
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(1),
-      width: "auto",
-    },
-  }));
-
-  const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  }));
-
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: "inherit",
-    "& .MuiInputBase-input": {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create("width"),
-      width: "100%",
-      [theme.breakpoints.up("sm")]: {
-        width: "12ch",
-        "&:focus": {
-          width: "20ch",
-        },
-      },
-    },
-  }));
   const darkTheme = createTheme({
     palette: {
       mode: "dark",
@@ -61,44 +43,86 @@ function Header() {
     },
   });
 
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery("(max-width:900px)");
+  const items = ["Home", "Dresses", "Jwellery"];
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   return (
     <ThemeProvider theme={darkTheme}>
       <AppBar position="sticky">
         <Toolbar>
-          <Grid container spacing={1}>
-            <Grid item xs={12} md={2.5}>
-              <Typography variant="h5" textAlign={"center"}>
-                Ethnic Gems
-              </Typography>
+          {!isSmallScreen && (
+            <Grid container spacing={1}>
+              <Grid item md={2.5}>
+                <Typography variant="h5">Ethnic Gems</Typography>
+              </Grid>
+              <Grid item md={1.25}>
+                <Typography variant="body1" textalign={"center"} mt={0.7}>
+                  Home
+                </Typography>
+              </Grid>
+              <Grid item md={1.25}>
+                <Typography variant="body1" textalign={"center"} mt={0.7}>
+                  Dresses
+                </Typography>
+              </Grid>
+
+              <Grid item md={1.25}>
+                <Typography variant="body1" textalign={"center"} mt={0.7}>
+                  Jewellery
+                </Typography>
+              </Grid>
+              <Grid item md={4}></Grid>
+              <Grid item md={1} mt={0.7}>
+                <ShoppingCartIcon size="large" textalign={"center"} />
+              </Grid>
             </Grid>
-            <Grid item xs={6} md={1.5}>
-              <Typography variant="body1" textAlign={"center"} mt={0.7}>
-                Dresses
-              </Typography>
-            </Grid>
-            <Grid item xs={6} md={1.5}>
-              <Typography variant="body1" textAlign={"center"} mt={0.7}>
-                Jewellery
-              </Typography>
-            </Grid>
-            <Grid item xs={12} md={3.5}>
-              <Search>
-                <SearchIconWrapper>
-                  <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase
-                  placeholder="Search…"
-                  inputProps={{ "aria-label": "search" }}
-                />
-              </Search>
-            </Grid>
-            <Grid item xs={6} md={2}></Grid>
-            <Grid item xs={6} md={1} mt={0.7}>
-              <ShoppingCartIcon size="large" textAlign={"center"} />
-            </Grid>
-          </Grid>
+          )}
+
+          {isSmallScreen && (
+            <>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                edge="start"
+                sx={{ mr: 2, ...(open && { display: "none" }) }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h5">Ethnic Gems</Typography>
+            </>
+          )}
         </Toolbar>
       </AppBar>
+      <Drawer variant="persistent" anchor="left" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {items &&
+            items.map((item, index) => (
+              <ListItem key={index}>
+                <ListItemText primary={item} />
+              </ListItem>
+            ))}
+        </List>
+      </Drawer>
     </ThemeProvider>
   );
 }
