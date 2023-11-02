58981d7672586e00f71602aa486a9f60108bd5cf
Arasi Dobariya <arasidobariya7@gmail.com>
2023-11-02 09:19:04 -0400

    added image structure in productdetail page

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import ProductList from "./components/ProductList";
 import StickyFooter from "./components/Footer";
+import ProductDetail from "./components/ProductDetail";
 function App() {
   return (
     <div className="body">
       <Header />
+      <ProductDetail/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products/:id" element={<ProductList />} />
--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,5 +1,40 @@
-function ProductDetail() {
+import { Grid, Paper, Typography, Box } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
-    
+function ProductDetail() {
+  return (
+    <Box sx={{ margin: 5 }}>
+      <Box
+        sx={{
+          margin: 5,
+          display: "flex",
+        }}
+      >
+        <ArrowBackIcon sx={{ px: 1 }}></ArrowBackIcon>
+        <Typography>Back to Shop</Typography>
+      </Box>
+      <Paper
+        elevation={3}
+        sx={{
+          margin: 5,
+        }}
+      >
+        <Grid container spacing={1}>
+          <Grid item xs={4}>
+            <Paper>
+              <img
+                src="./image/Banner Girl.jpg"
+                height="450px"
+                width="100%"
+                alt="chaniyacholi pic"
+              />
+            </Paper>
+          </Grid>
+
+          <Grid item xs={6}></Grid>
+        </Grid>
+      </Paper>
+    </Box>
+  );
 }
 export default ProductDetail;
