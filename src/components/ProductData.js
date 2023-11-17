013465a37f7fffe558ba9a2c4066d2ae3ba11e30
Arasi Dobariya <arasidobariya7@gmail.com>
2023-11-16 20:11:39 -0500

    created a seperate file for product data and remove store provider

---
+++ src/components/ProductData.js
@@ -0,0 +1,111 @@
+const ProductData = [
+  {
+    id: 1,
+    category: "dresses",
+    src: "./image/dress1.jpg",
+    title: "Blue Dress",
+    price: "$100",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi. Lorem ipsum dolor sitamet consectetur adipisicing elit. Quaerat placeat similiquedicta nulla praesentium deserunt. Corporis repellendus delenitidolores eligendi.",
+  },
+  {
+    id: 2,
+    category: "dresses",
+    src: "./image/dress2.jpg",
+    title: "Blue Dress",
+    price: "$120",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi. Lorem ipsum dolor sitamet consectetur adipisicing elit. Quaerat placeat similiquedicta nulla praesentium deserunt. Corporis repellendus delenitidolores eligendi.",
+  },
+  {
+    id: 3,
+    category: "dresses",
+    src: "./image/dress3.jpg",
+    title: "Blue Dress",
+    price: "$110",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi. Lorem ipsum dolor sitamet consectetur adipisicing elit. Quaerat placeat similiquedicta nulla praesentium deserunt. Corporis repellendus delenitidolores eligendi.",
+  },
+  {
+    id: 4,
+    category: "dresses",
+    src: "./image/dress4.jpg",
+    title: "Blue Dress",
+    price: "$90",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi. Lorem ipsum dolor sitamet consectetur adipisicing elit. Quaerat placeat similiquedicta nulla praesentium deserunt. Corporis repellendus delenitidolores eligendi.",
+  },
+  {
+    id: 5,
+    category: "dresses",
+    src: "./image/dress5.jpg",
+    title: "Blue Dress",
+    price: "$140",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi. Lorem ipsum dolor sitamet consectetur adipisicing elit. Quaerat placeat similiquedicta nulla praesentium deserunt. Corporis repellendus delenitidolores eligendi.",
+  },
+  {
+    id: 6,
+    category: "dresses",
+    src: "./image/dress6.jpg",
+    title: "Blue Dress",
+    price: "$70",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi. Lorem ipsum dolor sitamet consectetur adipisicing elit. Quaerat placeat similiquedicta nulla praesentium deserunt. Corporis repellendus delenitidolores eligendi.",
+  },
+  {
+    id: 7,
+    category: "Jewellery",
+    src: "./image/Jewellery1.jpg",
+    title: "Blue Dress",
+    price: "$100",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi. Lorem ipsum dolor sitamet consectetur adipisicing elit. Quaerat placeat similiquedicta nulla praesentium deserunt. Corporis repellendus delenitidolores eligendi.",
+  },
+  {
+    id: 8,
+    category: "Jewellery",
+    src: "./image/Jewellery2.jpg",
+    title: "Blue Dress",
+    price: "$150",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi. Lorem ipsum dolor sitamet consectetur adipisicing elit. Quaerat placeat similiquedicta nulla praesentium deserunt. Corporis repellendus delenitidolores eligendi.",
+  },
+  {
+    id: 9,
+    category: "Jewellery",
+    src: "./image/Jewellery3.jpg",
+    title: "Blue Dress",
+    price: "$160",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi. Lorem ipsum dolor sitamet consectetur adipisicing elit. Quaerat placeat similiquedicta nulla praesentium deserunt. Corporis repellendus delenitidolores eligendi.",
+  },
+  {
+    id: 10,
+    category: "Jewellery",
+    src: "./image/Jewellery4.jpg",
+    title: "Blue Dress",
+    price: "$100",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi. Lorem ipsum dolor sitamet consectetur adipisicing elit. Quaerat placeat similiquedicta nulla praesentium deserunt. Corporis repellendus delenitidolores eligendi.",
+  },
+  {
+    id: 11,
+    category: "Jewellery",
+    src: "./image/Jewellery5.jpg",
+    title: "Blue Dress",
+    price: "$170",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi. Lorem ipsum dolor sitamet consectetur adipisicing elit. Quaerat placeat similiquedicta nulla praesentium deserunt. Corporis repellendus delenitidolores eligendi.",
+  },
+  {
+    id: 12,
+    category: "Jewellery",
+    src: "./image/Jewellery6.jpg",
+    title: "Blue Dress",
+    price: "$150",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi. Lorem ipsum dolor sitamet consectetur adipisicing elit. Quaerat placeat similiquedicta nulla praesentium deserunt. Corporis repellendus delenitidolores eligendi.",
+  },
+];
+export default ProductData;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,19 +1,16 @@
 import { Grid, Box, Paper, Typography } from "@mui/material";
 import { useParams } from "react-router";
-import { productDataActions } from "../Store/store";
-import { useDispatch, useSelector } from "react-redux";
+
+import ProductData from "./ProductData";
 
 function ProductList() {
   const param = useParams();
-  const dispatch = useDispatch();
-  const items = useSelector((state) => state.ProductList.items);
 
   return (
     <Box sx={{ margin: 5 }}>
       <Grid container spacing={5}>
-        {items
-          .filter((item) => item.category === param.id)
-          .map((item) => {
+        {ProductData.filter((item) => item.category === param.id).map(
+          (item) => {
             return (
               <Grid
                 item
@@ -30,15 +27,14 @@ function ProductList() {
                 <Paper variant="outlined" sx={{ maxWidth: "500px" }}>
                   <Grid item>
                     <img
-                      height="200px"
-                      width="200px"
+                       src={item.src}
                       alt={item.title}
-                      src={item.src}
+                     
                     ></img>
                   </Grid>
                   <Grid item sx={{ margin: 2 }}>
                     <Typography variant="h5">{item.title}</Typography>
-                    <Typography variant="body2">{item.description}</Typography>
+                    <Typography variant="body2">{item.price}</Typography>
                   </Grid>
 
                   <Typography
@@ -56,7 +52,8 @@ function ProductList() {
                 </Paper>
               </Grid>
             );
-          })}
+          }
+        )}
       </Grid>
     </Box>
   );
